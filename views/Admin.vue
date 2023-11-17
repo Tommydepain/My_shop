67036c099019b54978af550ff4ae9babce6d55cd
LouisonLongprez <146077027+LouisonLongprez@users.noreply.github.com>
2023-11-17 20:21:14 +0100

    Add files via upload

--- views/Admin.vue
+++ views/Admin.vue
@@ -1,8 +1,18 @@
 <template>
-    <!-- html -->
-    <html lang="en">
-    <head> 
-<router-link to="/admin" id="login-button" ms-hide-element="true" href="" class="button admin w-button">Admin</router-link>
-    </head>
-    </html>
-    </template>
+    <section>
+        <li><h1>jgvqs</h1></li>
+        <Admin_comp/>
+    </section>
+</template>
+    
+<script>
+    import Admin_comp from '../components/Admin_comp.vue'
+ 
+    export default {
+    components: {
+        Admin_comp,
+        }
+    }
+</script>
+
+<!-- id="login-button" ms-hide-element="true" href="" class="button admin w-button" -->
