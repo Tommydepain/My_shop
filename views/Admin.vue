415ad6e6711139a23736df5051309693abeb43b6
LouisonLongprez <146077027+LouisonLongprez@users.noreply.github.com>
2023-11-17 11:18:30 +0100

    Add files via upload

---
+++ views/Admin.vue
@@ -0,0 +1,8 @@
+<template>
+    <!-- html -->
+    <html lang="en">
+    <head> 
+<router-link to="/admin" id="login-button" ms-hide-element="true" href="" class="button admin w-button">Admin</router-link>
+    </head>
+    </html>
+    </template>
